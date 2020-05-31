2841ffb194b08b83055ced20c35164651ea0868e
girkovarpa@protonmail.com <GirkovArpa>
2020-05-31 14:04:11 -0500

    MVP formula

--- index.js
+++ index.js
@@ -44,13 +44,13 @@ console.log('subjects: ', subjects);
 console.log('-------------');
 console.log('students: ', students);
 
-const days = new Array(9); for (let i = 0; i < days.length; i++) days[i] = [];
+const days = new Array(5); for (let i = 0; i < days.length; i++) days[i] = [];
 
 for (let i = 0; i < days.length; i++) {
   for (const student of Object.values(students)) {
     if (student.tired) {
       student.tired = false;
-      continue;
+      if ((days.length - i) > student.subjects.length) continue;
     }
     for (const subject of student.subjects) {
       days[i].push({ number: student.number, subject });
