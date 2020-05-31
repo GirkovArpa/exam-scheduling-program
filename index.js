040a89c0395b2f9fea69aaa87c65e88a123e6b49
girkovarpa@protonmail.com <GirkovArpa>
2020-05-31 13:07:30 -0500

    schedule 1 subject per student per 2 days

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 const data = [
   { No: 1234567, Subj: 'ACCY' },
   { No: 1234567, Subj: 'ECON' },
@@ -11,6 +18,8 @@ const data = [
   { No: 1234569, Subj: 'ACCY' },
   { No: 1234569, Subj: 'ECON' }
 ];
+
+shuffleArray(data);
 
 const students = {};
 for (const datum of data) {
@@ -24,18 +33,26 @@ for (const datum of data) {
   subjects[datum.Subj].students.push(datum.No);
 }
 
+console.log(subjects);
+console.log('-------------');
 console.log(students);
 
 const days = new Array(9); for (let i = 0; i < days.length; i++) days[i] = [];
 
 for (let i = 0; i < days.length; i++) {
   for (const student of Object.values(students)) {
+    if (student.tired) {
+      student.tired = false;
+      continue;
+    }
     for (const subject of student.subjects) {
       days[i].push({ number: student.number, subject });
       student.subjects.shift();
+      student.tired = true;
       break;
     }
   }
 }
 
+console.log('-------');
 console.log(days);
