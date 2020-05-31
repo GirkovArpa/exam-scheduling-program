7521cd5cbec1dd8f9e8077846f50aec3a6c11534
girkovarpa@protonmail.com <GirkovArpa>
2020-05-31 13:03:04 -0500

    schedule students 1 subj per day

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 'use strict';
 
+const data = [
+  { No: 1234567, Subj: 'ACCY' },
+  { No: 1234567, Subj: 'ECON' },
+  { No: 1234567, Subj: 'STAT' },
+  { No: 1234568, Subj: 'ACCY' },
+  { No: 1234568, Subj: 'ECON' },
+  { No: 1234568, Subj: 'PHY' },
+  { No: 1234568, Subj: 'FIN' },
+  { No: 1234569, Subj: 'ACCY' },
+  { No: 1234569, Subj: 'ECON' }
+];
 
-
-const data = `No. Subj 1234567 ACCY 1234567 ECON 1234567 STAT 1234568 ACCY 1234568 ECON 1234568 PHY 1234568 FIN 1234569 ACCY 1234569 ECON`
-  .split(' ');
-
-const output = [];
-for (let i = 2; i < data.length; i++) {
-  const No = data[i];
-  const Subj = data[++i];
-  const student = { No, Subj };
-  output.push(student);
+const students = {};
+for (const datum of data) {
+  if (!students[datum.No]) students[datum.No] = { number: datum.No, subjects: [] };
+  students[datum.No].subjects.push(datum.Subj);
 }
-console.log(output);
+
+const subjects = {};
+for (const datum of data) {
+  if (!subjects[datum.Subj]) subjects[datum.Subj] = { subject: datum.Subj, students: [] };
+  subjects[datum.Subj].students.push(datum.No);
+}
+
+console.log(students);
+
+const days = new Array(9); for (let i = 0; i < days.length; i++) days[i] = [];
+
+for (let i = 0; i < days.length; i++) {
+  for (const student of Object.values(students)) {
+    for (const subject of student.subjects) {
+      days[i].push({ number: student.number, subject });
+      student.subjects.shift();
+      break;
+    }
+  }
+}
+
+console.log(days);
